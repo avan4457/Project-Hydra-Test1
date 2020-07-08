de53a5677aa438aacfa0a983c1a4f6001c8266fd
Avantha <avanthac@yahoo.com>
2020-07-08 22:29:17 +0530

    files add

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const exp = require('express'); //importing express module
 const port = process.env.PORT || 3000; //defining port
 const router = require('./router/routes'); //importing router
 
-let app = exp(); 
+let app = exp(); // creating express application
 
-app.set('view engine','ejs');
+app.set('view engine','ejs'); // defining the view engine that is being used
 
-app.use('/',router);
+app.use('/',router); // using routes
 
-app.listen(port,(err)=>{
+app.use(exp.static(__dirname + '/public')); // defining static files that are used in public folder
+
+app.listen(port,(err)=>{ // Starting server and listen through the defined port
     if(err) throw err;
     console.log('Listening on port '+ port);
 })
